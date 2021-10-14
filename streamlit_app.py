13e6c0a70ca37b34f908e7879da0621ea552cd0e
aalevine <aalbertlevine@gmail.com>
2021-10-14 15:57:53 +0200

    add password requirement

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,41 +3,59 @@ import streamlit as st
 import altair as alt
 import pandas as pd
 import sheets as sh
-import numpy as np
+from bokeh.models import Div
 
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
-SPREADSHEET_ID = '1MYdAevCV4bvw73-h925A1Hlt5Gadgcm6pEFcxYGtvXg'
-DATA_TO_PULL = 'Export!A1:F25'
+# Get password from user
+password = st.sidebar.text_input("Password:", value="")
 
-data = sh.pull_sheet_data(SCOPES, SPREADSHEET_ID, DATA_TO_PULL)
-df = pd.DataFrame(data[1:], columns=data[0])
+# select our text input field and make it into a password input
+js = "el = document.querySelectorAll('.sidebar-content input')[0]; el.type = 'password';"
 
-st.markdown("# caseWhen Consulting :man-surfing:")
-st.markdown("")
-st.markdown("### Up and to the Right :chart_with_upwards_trend:")
+# passing js code to the onerror handler of an img tag with no src
+# triggers an error and allows automatically running our code
+html = f'<img src onerror="{js}">'
 
-# df3 = pd.DataFrame({
-#   'Month': df['Month'].values,
-#   'Cumulative Profit': [int(x.replace(',', '')) for x in df['Cumulative Profit'].values]
-# })
-#
-# bar_chart = alt.Chart(df3).mark_bar().encode(
-#     alt.X('Month', title=''),
-#     alt.Y('Cumulative Profit', title='Cumulative Profit')
-# )
-#
-# st.altair_chart(bar_chart)
+# in contrast to st.write, this seems to allow passing javascript
+div = Div(text=html)
+st.bokeh_chart(div)
 
-# Example with Streamlit wrapper
-df2 = pd.DataFrame({
-  'Month': df['Month'].values,
-  'Cumulative Profit': [int(x.replace(',', '')) for x in df['Cumulative Profit'].values]
-})
-df2 = df2.set_index('Month')
+if password != st.secrets["PASSWORD"]:
+    st.error("the password you entered is incorrect")
 
-st.bar_chart(data=df2)
-# AgGrid(df2)
+else:
+    SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+    SPREADSHEET_ID = '1MYdAevCV4bvw73-h925A1Hlt5Gadgcm6pEFcxYGtvXg'
+    DATA_TO_PULL = 'Export!A1:F25'
 
-st.markdown("### Financials :moneybag:")
-AgGrid(df)
+    data = sh.pull_sheet_data(SCOPES, SPREADSHEET_ID, DATA_TO_PULL)
+    df = pd.DataFrame(data[1:], columns=data[0])
+
+    st.markdown("# caseWhen Consulting :man-surfing:")
+    st.markdown("")
+    st.markdown("### Up and to the Right :chart_with_upwards_trend:")
+
+    # df3 = pd.DataFrame({
+    #   'Month': df['Month'].values,
+    #   'Cumulative Profit': [int(x.replace(',', '')) for x in df['Cumulative Profit'].values]
+    # })
+    #
+    # bar_chart = alt.Chart(df3).mark_bar().encode(
+    #     alt.X('Month', title=''),
+    #     alt.Y('Cumulative Profit', title='Cumulative Profit')
+    # )
+    #
+    # st.altair_chart(bar_chart)
+
+    # Example with Streamlit wrapper
+    df2 = pd.DataFrame({
+      'Month': df['Month'].values,
+      'Cumulative Profit': [int(x.replace(',', '')) for x in df['Cumulative Profit'].values]
+    })
+    df2 = df2.set_index('Month')
+
+    st.bar_chart(data=df2)
+    # AgGrid(df2)
+
+    st.markdown("### Financials :moneybag:")
+    AgGrid(df)
